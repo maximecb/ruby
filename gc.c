86cd7cc83fa3d2ff576ecab1fa3b49261fff8553
Aaron Patterson <tenderlove@ruby-lang.org>
2021-03-12 17:04:20 -0800

    Ooops, did my math wrong
    We need to move forward by the length times the size of the stride.

--- gc.c
+++ gc.c
@@ -9689,7 +9689,7 @@ gc_ref_update(void *vstart, void *vend, size_t stride, rb_objspace_t * objspace,
           case T_ZOMBIE:
             break;
           case T_PAYLOAD:
-              v += (RPAYLOAD(v)->len - 1);
+              v += (stride * (RPAYLOAD(v)->len - 1));
             break;
           default:
             if (RVALUE_WB_UNPROTECTED(v)) {
