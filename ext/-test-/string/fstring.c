54de2bdbd0997d38bf2227ee5f722d5dcda76be7
Jean Boussier <jean.boussier@gmail.com>
2021-03-18 17:54:51 +0100

    Add a failing test case for rb_enc_interned_str and Windows-31-J

--- ext/-test-/string/fstring.c
+++ ext/-test-/string/fstring.c
@@ -1,4 +1,5 @@
 #include "ruby.h"
+#include "ruby/encoding.h"
 
 VALUE rb_fstring(VALUE str);
 
@@ -8,8 +9,15 @@ bug_s_fstring(VALUE self, VALUE str)
     return rb_fstring(str);
 }
 
+VALUE
+bug_s_rb_enc_interned_str_windows_31_j(VALUE self)
+{
+    return rb_enc_interned_str("foo", 3, rb_enc_from_index(11 /* Windows-31-J */));
+}
+
 void
 Init_string_fstring(VALUE klass)
 {
     rb_define_singleton_method(klass, "fstring", bug_s_fstring, 1);
+    rb_define_singleton_method(klass, "rb_enc_interned_str_windows_31_j", bug_s_rb_enc_interned_str_windows_31_j, 0);
 }
