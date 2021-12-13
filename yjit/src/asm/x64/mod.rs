2c850800879cd56022adafbd25000507dd23fd10
Alan Wu <XrXr@users.noreply.github.com>
2021-12-13 16:12:04 -0500

    WIP: new design for assembler using traits
    Found an interesting gocha with panic! in constant context.

--- yjit/src/asm/x64/mod.rs
+++ yjit/src/asm/x64/mod.rs
@@ -1,34 +1,78 @@
-/// An x64 assembler with Rust interface.
-/// Warning: incomplete and barely tested.
+//! An x64 assembler with Rust interface.
+//! Warning: incomplete and barely tested.
 
-/// x64 general purpose register
-#[derive(Debug)]
-pub struct Register {
-    /// Revision of the ISA in which the register first appeared
-    vintage: RegisterVintage,
-
+/// A type implementing this trait groups together general purpose register of a certain bit width.
+pub trait Register {
     /// Bit width of the register
-    width: RegisterWidth,
+    const WIDTH: RegisterWidth;
 
-    /// Number for encoding the register
-    // Design note: Rust doesn't have refinement types, but we
-    // could make do something like enum ZeroToSeven { Zero=0, ... }
-    // if we want to go for absolute type safety.
-    id: u8,
+    /// The bit used to identify the register in the REX byte.
+    /// This bit is set for R9-R15, for example.
+    fn id_rex_bit(&self) -> u8;
+
+    /// The 3 bit number for identifying the register in the modr/m byte.
+    fn id_rm(&self) -> u8;
 }
 
-/// Groupings of registers with encoding significance
-#[derive(Debug, PartialEq)]
-enum RegisterVintage {
-    /// The register is in the original x86 ISA
-    Original,
-    /// The register first appeared in the amd64 ISA
-    Extended,
+pub struct Reg64(RegId);
+pub struct Reg32(RegId);
+pub struct Reg16(RegId);
+pub struct Reg8(RegId);
+
+pub struct RegId {
+    id_rex_bit: u8,
+    id_rm: u8,
+}
+
+use RegisterWidth::*;
+
+impl Register for Reg64 {
+    const WIDTH: RegisterWidth = B64;
+
+    fn id_rex_bit(&self) -> u8 {
+        self.0.id_rex_bit
+    }
+    fn id_rm(&self) -> u8 {
+        self.0.id_rm
+    }
+}
+
+impl Register for Reg32 {
+    const WIDTH: RegisterWidth = B32;
+
+    fn id_rex_bit(&self) -> u8 {
+        self.0.id_rex_bit
+    }
+    fn id_rm(&self) -> u8 {
+        self.0.id_rm
+    }
+}
+
+impl Register for Reg16 {
+    const WIDTH: RegisterWidth = B16;
+
+    fn id_rex_bit(&self) -> u8 {
+        self.0.id_rex_bit
+    }
+    fn id_rm(&self) -> u8 {
+        self.0.id_rm
+    }
+}
+
+impl Register for Reg8 {
+    const WIDTH: RegisterWidth = B8;
+
+    fn id_rex_bit(&self) -> u8 {
+        self.0.id_rex_bit
+    }
+    fn id_rm(&self) -> u8 {
+        self.0.id_rm
+    }
 }
 
 /// Bit width of register
-#[derive(Debug, PartialEq)]
-enum RegisterWidth {
+#[derive(Debug, PartialEq, Eq)]
+pub enum RegisterWidth {
     B8,
     B16,
     B32,
@@ -39,50 +83,35 @@ enum RegisterWidth {
 /// For simplificty, high byte registers such as AH are excluded on purpose.
 macro_rules! general_purpose_registers {
     (
-        $($vintage:ident $id:literal $b8_name:ident $b16_name:ident $b32_name:ident $b64_name:ident)*
+        $(rex:$vintage:literal $id:literal $b8_name:ident $b16_name:ident $b32_name:ident $b64_name:ident)*
     ) => {
-        use RegisterVintage::*;
-        use RegisterWidth::*;
         $(
-            pub const $b64_name: Register = Register { vintage: $vintage, width: B64, id: $id };
-            pub const $b32_name: Register = Register { vintage: $vintage, width: B32, id: $id };
-            pub const $b16_name: Register = Register { vintage: $vintage, width: B16, id: $id };
-            pub const $b8_name: Register = Register { vintage: $vintage, width: B8, id: $id };
+            pub const $b64_name: Reg64 = Reg64(RegId{ id_rex_bit: $vintage, id_rm: $id });
+            pub const $b32_name: Reg32 = Reg32(RegId { id_rex_bit: $vintage, id_rm: $id });
+            pub const $b16_name: Reg16 = Reg16(RegId { id_rex_bit: $vintage, id_rm: $id });
+            pub const $b8_name: Reg8 = Reg8(RegId { id_rex_bit: $vintage, id_rm: $id });
         )*
     }
 }
 
+// x64 general purpose register
 general_purpose_registers! {
-    Original 0 AL   AX   EAX  RAX
-    Original 1 CL   CX   ECX  RCX
-    Original 2 DL   DX   EDX  RDX
-    Original 3 BL   BX   EBX  RBX
-    Original 4 SPL  SP   ESP  RSP
-    Original 5 BPL  BP   EBP  RBP
-    Original 6 SIL  SI   ESI  RSI
-    Original 7 DIL  DI   EDI  RDI
-    Extended 0 R8L  R8W  R8D  R8
-    Extended 1 R9L  R9W  R9D  R9
-    Extended 2 R10L R10W R10D R10
-    Extended 3 R11L R11W R11D R11
-    Extended 4 R12L R12W R12D R12
-    Extended 5 R13L R13W R13D R13
-    Extended 6 R14L R14W R14D R14
-    Extended 7 R15L R15W R15D R15
-}
-
-/// Allows for usages such as mov(RAX.into(), RBX.into())
-impl From<Register> for Operand {
-    fn from(reg: Register) -> Self {
-        Operand::Register(reg)
-    }
-}
-
-/// Allows for usages such as mov(RAX.into(), 1.into())
-impl From<i32> for Operand {
-    fn from(i: i32) -> Self {
-        Operand::Immediate(RegWidthInteger::I32(i))
-    }
+    rex:0 0 AL   AX   EAX  RAX
+    rex:0 1 CL   CX   ECX  RCX
+    rex:0 2 DL   DX   EDX  RDX
+    rex:0 3 BL   BX   EBX  RBX
+    rex:0 4 SPL  SP   ESP  RSP
+    rex:0 5 BPL  BP   EBP  RBP
+    rex:0 6 SIL  SI   ESI  RSI
+    rex:0 7 DIL  DI   EDI  RDI
+    rex:1 0 R8L  R8W  R8D  R8
+    rex:1 1 R9L  R9W  R9D  R9
+    rex:1 2 R10L R10W R10D R10
+    rex:1 3 R11L R11W R11D R11
+    rex:1 4 R12L R12W R12D R12
+    rex:1 5 R13L R13W R13D R13
+    rex:1 6 R14L R14W R14D R14
+    rex:1 7 R15L R15W R15D R15
 }
 
 #[derive(Debug)]
@@ -97,19 +126,6 @@ pub enum RegWidthInteger {
     U64(u64),
 }
 
-use RegWidthInteger::*;
-
-/// Operand for x64 instructions
-#[derive(Debug)]
-pub enum Operand {
-    Register(Register),
-    Immediate(RegWidthInteger),
-    //AddressingForm,
-    //Label,
-    //Address(usize),
-    //IPRelative(),
-}
-
 /// x64 assembler
 pub struct Assembler {
     /// The encoded bytes for emitted instructions
@@ -117,36 +133,41 @@ pub struct Assembler {
 }
 
 /// Temporary
-struct Encoding {
+pub struct Encoding {
     rex: Option<u8>,
     opcode: u8,
     modrm: u8,
-    imm32: i32,
+    imm32: Option<i32>, // Tmporary. Maybe a enum with different imm sizes
 }
 
-mod InstructionForms {
+mod mnemonic_forms {
     use crate::asm::x64::Encoding;
     pub trait Test {
-        fn encode(&self) -> Encoding;
+        const ACCEPTABLE: ();
+        fn encode(self) -> Encoding;
     }
 }
 
-impl InstructionForms::Test for (Register, i32) {
-    fn encode(&self) -> Encoding {
+impl<Reg: Register> mnemonic_forms::Test for (Reg, i32) {
+    const ACCEPTABLE: () = match Reg::WIDTH {
+        B64 | B32 => (),
+        _ => panic!("Only Reg64 and Reg32 for now"),
+    };
+    fn encode(self) -> Encoding {
+        // NOTE(alan): it's surprising that the associated constant
+        // is not evaluated unless used. Timie to file a bug?
+        let _: () = Self::ACCEPTABLE;
         let rex = {
             //TODO comment
-            let rex_w = (self.0.width == B64) as u8;
+            let rex_w = (Reg::WIDTH == B64) as u8;
             let rex_r = 0;
             let rex_x = 0;
-            let rex_b = (self.0.vintage == Extended) as u8;
+            let rex_b = self.0.id_rex_bit();
             if (rex_w, rex_r, rex_x, rex_b) != (0, 0, 0, 0) {
                 // <- most significant bit
                 // 0 1 0 0 W R X B
-                let rex = 0b0100_0000
-                    + 0b1000 * rex_w
-                    + 0b0100 * rex_r
-                    + 0b0010 * rex_x
-                    + 0b0001 * rex_b;
+                let rex =
+                    0b0100_0000 + 0b1000 * rex_w + 0b0100 * rex_r + 0b0010 * rex_x + 0b0001 * rex_b;
                 Some(rex)
             } else {
                 None
@@ -156,8 +177,39 @@ impl InstructionForms::Test for (Register, i32) {
         Encoding {
             rex: rex,
             opcode: 0xf7,
-            modrm: 0b11000000 + self.0.id,
-            imm32: self.1,
+            modrm: 0b11000000 + self.0.id_rm(),
+            imm32: Some(self.1),
+        }
+    }
+}
+
+impl<Reg: Register> mnemonic_forms::Test for (Reg, Reg) {
+    const ACCEPTABLE: () = ();
+    fn encode(self) -> Encoding {
+        let (lhs, rhs) = self;
+
+        // Decide on the REX byte
+        let rex = {
+            let rex_w = (Reg::WIDTH == B64) as u8;
+            let rex_r = rhs.id_rex_bit();
+            let rex_x = (false) as u8;
+            let rex_b = lhs.id_rex_bit();
+            if (rex_w, rex_r, rex_x, rex_b) != (0, 0, 0, 0) {
+                // <- most significant bit
+                // 0 1 0 0 W R X B
+                let rex =
+                    0b0100_0000 + 0b1000 * rex_w + 0b0100 * rex_r + 0b0010 * rex_x + 0b0001 * rex_b;
+                Some(rex)
+            } else {
+                None
+            }
+        };
+
+        Encoding {
+            rex: rex,
+            opcode: 0x85,
+            modrm: 0b11_000_000 + (rhs.id_rm() << 3) + (lhs.id_rm() << 0),
+            imm32: None,
         }
     }
 }
@@ -170,6 +222,7 @@ impl Assembler {
         &self.encoded
     }
 
+    /*
     /// Right arithmetic shift
     /// TODO: what I want for rhs is really a RegOrImm type. Probably reusable.
     /// Maybe a RegOrMem type for lhs too? Look for asserts at the top of test(cb) for example.
@@ -218,20 +271,23 @@ impl Assembler {
             }
         }
     }
+    */
 
-    pub fn test<T: InstructionForms::Test>(&mut self, operands: T) {
+    pub fn test<T: mnemonic_forms::Test>(&mut self, operands: T) {
         let encoding = operands.encode();
         if let Some(rex) = encoding.rex {
             self.encoded.push(rex);
         }
         self.encoded.push(encoding.opcode);
         self.encoded.push(encoding.modrm);
-        self.encoded.push(encoding.imm32 & 0xFF);
-        self.encoded.push((encoding.imm32 & 0xFF00) >> 8);
-        self.encoded.push((encoding.imm32 & 0xFF0000) >> 16);
-        self.encoded.push((encoding.imm32 & 0xFF000000) >> 24);
+        if let Some(imm32) = encoding.imm32 {
+            for byte in imm32.to_le_bytes() {
+                self.encoded.push(byte);
+            }
+        }
     }
 
+    /*
     pub fn mov(&mut self, dst: Operand, src: Operand) {
         match (dst, src) {
             (Operand::Register(dst), Operand::Register(src))
@@ -286,6 +342,7 @@ impl Assembler {
             }
         }
     }
+    */
 }
 
 #[cfg(test)]
@@ -302,6 +359,7 @@ mod tests {
         }
     }
 
+    /*
     #[test]
     fn reg_to_reg_movs() {
         let mut asm = Assembler::new();
@@ -344,16 +402,38 @@ mod tests {
 
         assert_eq!("48 d1 f8 49 d1 f9 48 d1 ff 41 d1 fa", asm.byte_string());
     }
+    */
 
     #[test]
     fn test() {
         let mut asm = Assembler::new();
 
-        // 64b
-        asm.test((RAX, 0b111));
+        // reg64, imm32
+        asm.test((RAX, i32::MAX));
+        asm.test((R11, 0x0FABCAFE));
+        asm.test((RDI, -0xFABCAFE));
+        asm.test((R8, -1));
+
+        // reg32, imm32
+        asm.test((EDI, i32::MAX));
+        asm.test((R9D, 0x0FABCAFE));
+        asm.test((EDI, -0xFABCAFE));
+        asm.test((R9W, -1));
+
+        // reg64, reg64
+        asm.test((RAX, RDX));
+        asm.test((RCX, R11));
+        asm.test((R12, RBX));
+        asm.test((R15, R14));
+
+        // reg32, reg32
+        asm.test((EAX, EDX));
+        asm.test((ECX, R11D));
+        asm.test((R12D, EBX));
+        asm.test((R15D, R14D));
 
         // TODO: write panic tests
 
-        assert_eq!("48 d1 f8 49 d1 f9 48 d1 ff 41 d1 fa", asm.byte_string());
+        assert_eq!("48 f7 c0 07 00 00 00", asm.byte_string());
     }
 }
