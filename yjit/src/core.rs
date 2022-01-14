294cacc2d68e35ed0246f1f43ffaed1b3d1aeb8e
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2022-01-14 11:36:39 -0500

    Port more of yjit_core.h, add a CodePtr newtype

--- yjit/src/core.rs
+++ yjit/src/core.rs
@@ -1,3 +1,6 @@
+
+
+
 // NOTE: maybe the registers should be defined in another module
 // This module is primarily concerned with the BBV logic
 // We could also rename this module to bbv.rs
@@ -14,17 +17,18 @@
 //#define REG1 RCX
 //#define REG1_32 ECX
 
+
+
+
+
 // Maximum number of temp value types we keep track of
 const MAX_TEMP_TYPES: usize = 8;
 
 // Maximum number of local variable types we keep track of
 const MAX_LOCAL_TYPES: usize = 8;
 
-// Default versioning context (no type information)
-//#define DEFAULT_CTX ( (ctx_t){ 0 } )
-
 // Represent the type of a value (local/stack/self) in YJIT
-enum Type {
+pub enum Type {
     Unknown,
     Imm,
     Heap,
@@ -40,52 +44,35 @@ enum Type {
     String,
 }
 
-trait TypeTrait {
-    fn is_imm(&self) -> bool;
-    fn is_heap(&self) -> bool;
-}
-
-impl TypeTrait for Type {
+impl Type {
     fn is_imm(&self) -> bool {
         match self {
-            Type::Unknown => false,
             Type::Imm => true,
-            Type::Heap => false,
             Type::Nil => true,
             Type::True => true,
             Type::False => true,
             Type::Fixnum => true,
             Type::Flonum => true,
-            Type::Array => false,
-            Type::Hash => false,
             Type::ImmSymbol => true,
-            Type::HeapSymbol => false,
-            Type::String => false,
+            _ => false,
         }
     }
 
     fn is_heap(&self) -> bool {
         match self {
-            Type::Unknown => false,
-            Type::Imm => false,
             Type::Heap => true,
-            Type::Nil => false,
-            Type::True => false,
-            Type::False => false,
-            Type::Fixnum => false,
-            Type::Flonum => false,
             Type::Array => true,
             Type::Hash => true,
-            Type::ImmSymbol => false,
             Type::HeapSymbol => true,
             Type::String => true,
+            _ => false,
         }
     }
 }
 
 // Potential mapping of a value on the temporary stack to
 // self, a local variable or constant so that we can track its type
-enum TempMapping {
+pub enum TempMapping {
     Stack,              // Normal stack value
     SelfOpnd,           // Temp maps to the self operand
     Local { idx: u8 },  // Temp maps to a local variable with index
@@ -102,7 +89,7 @@ STATIC_ASSERT(temp_type_mapping_size, sizeof(temp_type_mapping_t) == 2);
 */
 
 // Operand to a bytecode instruction
-enum InsnOpnd {
+pub enum InsnOpnd {
     // The value is self
     SelfOpnd,
 
@@ -114,7 +101,7 @@ enum InsnOpnd {
 Code generation context
 Contains information we can use to optimize code
 */
-struct Ctx
+pub struct Ctx
 {
     // Number of values currently on the temporary stack
     stack_size : u16,
@@ -138,10 +125,21 @@ struct Ctx
     // Mapping of temp stack entries to types we track
     temp_mapping: [TempMapping; MAX_TEMP_TYPES],
 }
-//STATIC_ASSERT(yjit_ctx_size, sizeof(ctx_t) <= 32);
+
+// TODO:
+// Default versioning context (no type information)
+//#define DEFAULT_CTX ( (ctx_t){ 0 } )
+
+
+
+
+
+
+
+
 
 // Tuple of (iseq, idx) used to identify basic blocks
-struct BlockId
+pub struct BlockId
 {
     // FIXME: we need a proper pointer type here
 
@@ -154,7 +152,15 @@ struct BlockId
 }
 
 // Null block id constant
-const BLOCKID_NULL: BlockId = BlockId { iseq: 0, idx: 0 };
+pub const BLOCKID_NULL: BlockId = BlockId { iseq: 0, idx: 0 };
+
+/// Pointer to a piece of machine code
+/// We may later change this to wrap an u32
+struct CodePtr(*mut u8);
+
+// TODO: do we want constructor (new) or some from() methods for code pointers?
+impl CodePtr {
+}
 
 /// Branch code shape enumeration
 enum BranchShape
@@ -174,21 +180,17 @@ typedef void (*branchgen_fn)(codeblock_t* cb, uint8_t* target0, uint8_t* target1
 
 
 
-/**
-Store info about an outgoing branch in a code segment
-Note: care must be taken to minimize the size of branch_t objects
-*/
-struct BranchEntry
+/// Store info about an outgoing branch in a code segment
+/// Note: care must be taken to minimize the size of branch_t objects
+struct Branch
 {
-    /*
     // Block this is attached to
-    struct yjit_block_version *block;
+    block: Block,
 
     // TODO: Alan suggests a code pointer type
     // Positions where the generated code starts and ends
-    uint8_t *start_addr;
-    uint8_t *end_addr;
-    */
+    start_addr: CodePtr,
+    end_addr: CodePtr,
 
     // Context right after the branch instruction
     src_ctx : Ctx,
@@ -196,15 +198,14 @@ struct BranchEntry
     // Branch target blocks and their contexts
     targets: [BlockId; 2],
     target_ctxs: [Ctx; 2],
-    //struct yjit_block_version *blocks[2];
+    blocks: [Block; 2],
 
-    /*
     // Jump target addresses
-    uint8_t *dst_addrs[2];
+    dst_addrs: [CodePtr; 2],
 
+    // TODO
     // Branch code generation function
-    branchgen_fn gen_fn;
-    */
+    //branchgen_fn gen_fn;
 
     // Shape of the branch
     shape: BranchShape,
@@ -225,8 +226,6 @@ typedef struct {
 typedef rb_darray(cme_dependency_t) cme_dependency_array_t;
 
 typedef rb_darray(branch_t*) branch_array_t;
-
-typedef rb_darray(uint32_t) int32_array_t;
 */
 
 
@@ -235,84 +234,80 @@ typedef rb_darray(uint32_t) int32_array_t;
 
 
 
-/**
-Basic block version
-Represents a portion of an iseq compiled with a given context
-Note: care must be taken to minimize the size of block_t objects
-*/
-struct Block
+/// Basic block version
+/// Represents a portion of an iseq compiled with a given context
+/// Note: care must be taken to minimize the size of block_t objects
+pub struct Block
 {
-    /*
     // Bytecode sequence (iseq, idx) this is a version of
-    blockid_t blockid;
+    blockid: BlockId,
+
+    // Index one past the last instruction for this block in the iseq
+    end_idx: u32,
 
     // Context at the start of the block
-    ctx_t ctx;
+    ctx: Ctx,
 
     // Positions where the generated code starts and ends
-    uint8_t *start_addr;
-    uint8_t *end_addr;
+    start_addr: CodePtr,
+    end_addr: CodePtr,
 
+    // FIXME: these need to be references/pointers, but this is going
+    // to be problematic with the borrow checker.
+    //
     // List of incoming branches (from predecessors)
-    branch_array_t incoming;
+    incoming: Vec<Branch>,
 
     // List of outgoing branches (to successors)
     // Note: these are owned by this block version
-    branch_array_t outgoing;
+    outgoing: Vec<Branch>,
 
+    // FIXME: should these be code pointers instead?
     // Offsets for GC managed objects in the mainline code block
-    int32_array_t gc_object_offsets;
+    gc_object_offsets: Vec<u32>,
 
     // CME dependencies of this block, to help to remove all pointers to this
     // block in the system.
-    cme_dependency_array_t cme_dependencies;
+    //cme_dependency_array_t cme_dependencies;
 
-    // Code address of an exit for `ctx` and `blockid`. Used for block
-    // invalidation.
-    uint8_t *entry_exit;
-
-    // Index one past the last instruction in the iseq
-    uint32_t end_idx;
-    */
+    // Code address of an exit for `ctx` and `blockid`.
+    // Used for block invalidation.
+    entry_exit: CodePtr,
 }
 
-
-
-
-/*
 // Code generation state
-typedef struct JITState
+pub struct JITState
 {
     // Inline and outlined code blocks we are
     // currently generating code into
-    codeblock_t* cb;
-    codeblock_t* ocb;
+    //codeblock_t* cb;
+    //codeblock_t* ocb;
 
     // Block version being compiled
-    block_t *block;
+    block: Block,
 
     // Instruction sequence this is associated with
-    const rb_iseq_t *iseq;
+    //const rb_iseq_t *iseq;
 
     // Index of the current instruction being compiled
-    uint32_t insn_idx;
+    insn_idx: u32,
 
+    /*
     // Opcode for the instruction being compiled
     int opcode;
 
     // PC of the instruction being compiled
     VALUE *pc;
+    */
 
     // Side exit to the instruction being compiled. See :side-exit:.
-    uint8_t *side_exit_for_pc;
+    side_exit_for_pc: CodePtr,
 
     // Execution context when compilation started
     // This allows us to peek at run-time values
-    rb_execution_context_t *ec;
+    //rb_execution_context_t *ec;
 
     // Whether we need to record the code address at
     // the end of this bytecode instruction for global invalidation
-    bool record_boundary_patch_point;
-
-} jitstate_t;
-*/
+    record_boundary_patch_point : bool,
+}
