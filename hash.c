ae3d7521f23ea52c5a96c1f9bfbfb0a51a06e882
Aaron Patterson <tenderlove@ruby-lang.org>
2022-01-28 10:06:02 -0800

    Fire write barrier after hash has been written
    Before this change the write barrier was executed before the key and
    value were actually reachable via the Hash.  This could cause
    inconsistencies in object coloration which would lead to accidental
    collection of dup'd keys.
    
    Example:
    
    1. Object O is grey, Object P is white.
    2. Write barrier fires O -> P
    3. Write barrier does nothing
    4. Malloc happens, which starts GC
    5. GC colors O black
    6. P is written in to O (now we have O -> P reference)
    7. P is now accidentally treated as garbage

--- hash.c
+++ hash.c
@@ -1673,6 +1673,8 @@ struct update_arg {
     st_data_t arg;
     st_update_callback_func *func;
     VALUE hash;
+    VALUE key;
+    VALUE value;
 };
 
 typedef int (*tbl_update_func)(st_data_t *, st_data_t *, st_data_t, int);
@@ -1707,9 +1709,6 @@ tbl_update_modify(st_data_t *key, st_data_t *val, st_data_t arg, int existing)
       case ST_CONTINUE:
         if (!existing || *key != old_key || *val != old_value)
             rb_hash_modify(hash);
-        /* write barrier */
-        RB_OBJ_WRITTEN(hash, Qundef, *key);
-        RB_OBJ_WRITTEN(hash, Qundef, *val);
         break;
       case ST_DELETE:
         if (existing)
@@ -1727,9 +1726,17 @@ tbl_update(VALUE hash, VALUE key, tbl_update_func func, st_data_t optional_arg)
         .arg = optional_arg,
         .func = func,
         .hash = hash,
+        .key  = key,
+        .value = (VALUE)optional_arg,
     };
 
-    return rb_hash_stlike_update(hash, key, tbl_update_modify, (st_data_t)&arg);
+    int ret = rb_hash_stlike_update(hash, key, tbl_update_modify, (st_data_t)&arg);
+
+    /* write barrier */
+    RB_OBJ_WRITTEN(hash, Qundef, arg.key);
+    RB_OBJ_WRITTEN(hash, Qundef, arg.value);
+
+    return ret;
 }
 
 #define UPDATE_CALLBACK(iter_lev, func) ((iter_lev) > 0 ? func##_noinsert : func##_insert)
@@ -2875,6 +2882,7 @@ hash_aset_str(st_data_t *key, st_data_t *val, struct update_arg *arg, int existi
 {
     if (!existing && !RB_OBJ_FROZEN(*key)) {
 	*key = rb_hash_key_str(*key);
+        arg->key = *key;
     }
     return hash_aset(key, val, arg, existing);
 }
